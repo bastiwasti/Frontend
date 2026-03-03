38b058f72f297349954a128ab33b48a87b49e777
Events Gallery Developer <developer@eventsgallery.com>
2026-03-03 20:26:22 +0100

    Remove duplicate navigation from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "@/components/Navigation";
 import { AuthProvider } from "@/components/providers/session-provider";
 
 const geistSans = Geist({
@@ -30,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <Navigation />
           {children}
         </AuthProvider>
       </body>
